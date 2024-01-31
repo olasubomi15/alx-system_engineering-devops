d93ebc9e7680faa25f0bf48b19f698c12b7e08e6
root <olufadejiolasubomi15@gmail.com>
2024-01-31 12:21:14 -0800

    configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# # 1-install_a_package.pp
+
+class { 'python':
+  version => 'system',
 }
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
