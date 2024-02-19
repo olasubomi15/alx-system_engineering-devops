fbce14d630c55da738b7186556f4cf72883afe8b
root <olufadejiolasubomi15@gmail.com>
2024-02-19 10:36:17 -0800

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,31 @@
+#!/usr/bin/python3
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
-def fetch_employee_data(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print(f"Error: Unable to fetch data for employee ID {employee_id}")
-        sys.exit(1)
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    return user_data, todos_data
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-def display_todo_progress(employee_id):
-    user_data, todos_data = fetch_employee_data(employee_id)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-    employee_name = user_data["name"]
-    done_tasks = [task for task in todos_data if task["completed"]]
-    total_tasks = len(todos_data)
-
-    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
     for task in done_tasks:
-        print(f"\t{task['title']}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    display_todo_progress(employee_id)
-
+        print("\t {}".format(task.get('title')))
